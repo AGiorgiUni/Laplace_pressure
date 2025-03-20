08d6e274280750011301fae32207114dfd03d5b4
AGiorgiUni <alessandro.giorgi@studenti.unimi.it>
2025-03-20 15:59:22 +0100

    upload ray effect

--- Geometry.py
+++ Geometry.py
@@ -3,6 +3,43 @@ import sympy as sp
 import pandas as pd 
 import matplotlib.pyplot as plt
 
+#-----------------  SURFACE PARAMETRIZATION-----------
+
+def sphere_sp(x,y):
+    '''
+        Define the shape of the surface of the sphere using only simpy function as z(x,y)
+    '''
+    return  sp.sqrt(r**2 - x**2 - y**2)
+
+
+
+def cylinder_sp(x,y):
+    '''
+        Define the shape of the surface using only simpy function as z(x,y)
+    '''
+    return  sp.sqrt(r-x**2)
+
+def parametrization_sphere_sp(theta,phi):
+    '''
+        Define the shape of the sphere surface using only simpy function in spheric coordinates
+    '''
+    return sp.Matrix([r*sp.cos(theta)*sp.cos(phi), r*sp.cos(theta)*sp.sin(phi), r*sp.sin(theta)])
+
+
+def parametrization_cyl_sp(theta,z):
+    '''
+        Define the shape of the cylinder surface using only simpy function in cylindric coordinates
+    '''
+    return sp.Matrix([r*sp.cos(theta), r*sp.sin(theta), z])
+
+def cylinder_pert_sp(theta,z):
+    '''
+        Define the shape of the perturbated cylinder surface using only simpy function in cylindric coordinates
+    '''
+    return sp.Matrix([(r + b * sp.cos(k*z)) * sp.cos(theta), (r + b * sp.cos(k*z)) * sp.sin(theta)  ,  z])
+
+
+
 def surface_derivatives(param):
     '''
         Compute the derivatives of the surface z(x,y). 
@@ -23,22 +60,6 @@ def surface_derivatives(param):
 
 
 
-def sphere_sp(x,y):
-    '''
-        Define the shape of the surphace using only simpy function
-    '''
-    return  sp.sqrt(r**2 - x**2 - y**2)
-
-
-
-def cylinder_sp(x,y):
-    '''
-        Define the shape of the surphace using only simpy function
-    '''
-    return  sp.sqrt(r-x**2)
-
-
-
 def pressure_comp_xy(func_z,data):
     '''
         Compute the pressure (mean curvature) apart from a constant given a surface z(x,y). 
@@ -49,6 +70,7 @@ def pressure_comp_xy(func_z,data):
         Returns an array with the local pressure
     '''
     pressure=[]
+    x, y = sp.symbols('x y')
     df_dx,df_dy,df_dxx,df_dyy,df_dxy = surface_derivatives(func_z)
     for i in range(len(data)):
 
@@ -73,6 +95,7 @@ def pressure_comp(func_z,data):
         Returns an array with the local pressure
     '''
     pressure=[]
+    x, y = sp.symbols('x y')
     df_dx,df_dy,df_dxx,df_dyy,df_dxy = surface_derivatives(func_z)
     for i in range(len(data)):
 
@@ -98,3 +121,42 @@ def pressure_comp(func_z,data):
         pressure_val = (E*N + G*L - 2*F*M)/(E*G-F**2)
         pressure.append(pressure_val)
     return pressure
+
+def pressure_theory_comp(a,func_z):
+    '''
+        Compute the mean curvature given a surface parametrization as a math formula. 
+        
+        Arguments:
+            x, y: sympy simbol for the two main variables of the system
+            func_z: is the parametrization of the surface as (f(x,y),g(x,y),z(x,y)) using sympy functions
+            
+        Returns a sympy formula for the mean curvature
+    '''
+
+    x = a[0]
+    ym= a[1]
+    
+    f = func_z(a)
+
+    df_dx = sp.diff(f, x)
+    df_dy = sp.diff(f, y)
+    df_dxx = sp.diff(f, x,x)
+    df_dyy = sp.diff(f, y,y)
+    df_dxy = sp.diff(f, x,y)
+    
+
+    #computation of normal direction
+    normal = df_dx.cross(df_dy) 
+    normal = normal / normal.norm()
+
+    #computation of coefficients
+    E = df_dx.dot(df_dx)
+    F = df_dx.dot(df_dy)
+    G = df_dy.dot(df_dy)
+    L = df_dxx.dot(normal)
+    M = df_dxy.dot(normal)
+    N = df_dyy.dot(normal)
+
+    pressure_val = (E*N + G*L - 2*F*M)/(E*G-F**2)
+    
+    return pressure_val
